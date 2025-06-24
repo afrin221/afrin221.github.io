ec7202710319565e666bc81653b9f204e96fc16b
afrin221 <patanafrin67@gmail.com>
2025-06-24 21:58:51 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>My First Website</title>
+</head>
+<body>
+  <h1>Hello, I’m Afrin 👋</h1>
+  <p>This is my first website hosted using GitHub Pages.</p>
+
+  <!-- Optional: You can link to script.js -->
+  <script src="script.js"></script>
+</body>
+</html>
